98e60315d36fdfaeff841feb5b36ff3453be3996
langrenzhou <203666984LZDJ1.2.3>
2020-05-17 01:44:14 +0800

    登陆注册模块的设计

--- DataApi/app.js
+++ DataApi/app.js
@@ -28,19 +28,19 @@ app.all('/content', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*"); //设置允许跨域的域名，*代表允许任意域名跨域
     res.header("Access-Control-Allow-Headers", "content-type"); //允许的header类型
     res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
-    
-    const id=req.query.id
-    const page=parseInt(req.query.page-1)*10
+
+    const id = req.query.id
+    const page = parseInt(req.query.page - 1) * 10
     const check = `select * from t_articles where type_id=${id} limit ${page},10`;
-    link.query(check,function(err,result){
-        if(err){
-         console.log(err)
-        }else{
+    link.query(check, function (err, result) {
+        if (err) {
+            console.log(err)
+        } else {
             res.send(result)
             res.end(err)
         }
     })
-    
+
 
 })
 //请求总页数
@@ -48,18 +48,37 @@ app.all('/pages', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*"); //设置允许跨域的域名，*代表允许任意域名跨域
     res.header("Access-Control-Allow-Headers", "content-type"); //允许的header类型
     res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
-    const id=req.query.id
+    const id = req.query.id
     const check = `select * from t_articles where type_id=${id}`;
-    link.query(check,function(err,result){
-        if(err){
-         console.log(err)
-        }else{
+    link.query(check, function (err, result) {
+        if (err) {
+            console.log(err)
+        } else {
             res.send(result)
             res.end(err)
         }
     })
-    
 
+
+})
+app.all("/login",function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*"); //设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Headers", "content-type"); //允许的header类型
+    res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+    // req.on("end", (sd) => {
+    // // sd就是传递过来的参数
+    //     console.log("登录时监听到了")
+    // var data_login = ""
+    // var data_lign_one = JSON.parse(data_login += sd)
+    // console.log(data_lign_one)
+    // // 可以使用req.on去监听data事件来获取post请求的参数也可以用req.body获取请求的参数
+    // // var login_mysql = `select * from t_users where username='${req.body.name}' and password='${req.body.psd}'`;
+    // // lianjie.query(login_mysql, function (err, result) {
+    // //     res.send(result)
+    // //     res.end(err)
+    // // })
+    // })
+    console.log(express.json(req.body)())
 })
 app.listen('8080', function () {
     console.log('127.0.0.1:8080')
--- LeduTaro/src/pages/index/NavBar.jsx
+++ LeduTaro/src/pages/index/NavBar.jsx
@@ -1,22 +1,44 @@
 import Taro, { Component } from "@tarojs/taro"
-import { View, Text ,Button,Input} from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 import IndexJpg from '../../assets/index.gif'
 import Type from './type'
-import { AtModal, AtModalHeader, AtModalContent, AtModalAction } from "taro-ui"
+import { AtModal, AtModalContent, AtModalAction, AtTabs, AtTabsPane, AtInput } from "taro-ui"
 import axios from 'axios'
 
 class NavBar extends Component {
     constructor() {
         super()
         this.state = {
-            isOpened: false
+            isOpened: false,
+            current: 0,
+            tabList: [{ title: "登录" }, { title: "注册" }],
+            userName: '',
+            passWord: '',
+            nikName: '',
+            userEmail: ''
         }
     }
     componentDidMount() {
 
     }
-    login() {
-        this.setState({ isOpened: true })
+    handleClick(e) {
+        
+    }
+    loginRegister() {
+
+        if (this.state.current) {
+
+            console.log('此时是注册')
+        } else {
+            console.log('此时是登录')
+            axios.post("http://127.0.0.1:8080/login",JSON.stringify({
+                userName:this.state.userName,
+                passWord:this.state.passWord
+            }
+            )).then(res=>{
+                console.log(res)
+            })
+        }
     }
     render() {
         return (
@@ -37,16 +59,73 @@ class NavBar extends Component {
                 </View>
 
                 <View className='at-col at-col-1 login'>
-                    <View onClick={this.login.bind(this)}>
+                    <View onClick={() => {this.setState({ isOpened: !this.state.isOpened })}}>
                         <Text>登录/注册</Text>
                     </View>
                     <AtModal isOpened={this.state.isOpened}>
                         <AtModalContent>
                             <View>
-                               <Input></Input>
+                                <AtTabs current={this.state.current} scroll tabList={this.state.tabList} onClick={(e)=>{ this.setState({ current: e, userName: '', passWord: '', nikName: '', userEmail: '' })}}>
+                               
+                                    <AtTabsPane current={this.state.current} index={0}>
+                                        <AtInput
+                                            name='value'
+                                            title='用户名:'
+                                            type='text'
+                                            placeholder='请输入您的用户名或者昵称'
+                                            value={this.state.userName}
+                                            onChange={(value) => { this.setState({ userName: value }); return value }}
+                                        />
+                                        <AtInput
+                                            name='value'
+                                            title='密码:'
+                                            type='text'
+                                            placeholder='请输入您的密码'
+                                            value={this.state.passWord}
+                                            onChange={(value) => { this.setState({ passWord: value }); return value }}
+                                        />
+                                    </AtTabsPane>
+                                    <AtTabsPane current={this.state.current} index={1}>
+                                        <AtInput
+                                            name='value'
+                                            title='用户名:'
+                                            type='text'
+                                            placeholder='请输入您的用户名'
+                                            value={this.state.userName}
+                                            onChange={(value) => { this.setState({ userName: value }); return value }}
+                                        />
+                                        <AtInput
+                                            name='value'
+                                            title='昵称:'
+                                            type='text'
+                                            placeholder='请输入您的昵称'
+                                            value={this.state.nikName}
+                                            onChange={(value) => { this.setState({ nikName: value }); return value }}
+                                        />
+                                        <AtInput
+                                            name='value'
+                                            title='邮箱:'
+                                            type='text'
+                                            placeholder='请输入您的邮箱'
+                                            value={this.state.userEmail}
+                                            onChange={(value) => { this.setState({ userEmail: value }); return value }}
+                                        />
+                                        <AtInput
+                                            name='value'
+                                            title='密码:'
+                                            type='text'
+                                            placeholder='请输入您的密码'
+                                            value={this.state.passWord}
+                                            onChange={(value) => { this.setState({ passWord: value }); return value }}
+                                        />
+                                    </AtTabsPane>
+                                </AtTabs>
                             </View>
-                      </AtModalContent>
-                        <AtModalAction> <Button>取消</Button> <Button>确定</Button> </AtModalAction>
+                        </AtModalContent>
+                        <AtModalAction>
+                            <Button onClick={() => { this.setState({ isOpened: !this.state.isOpened }) }}>取消</Button>
+                            <Button onClick={this.loginRegister.bind(this)}>{this.state.current ? '注册' : "登录"}</Button>
+                        </AtModalAction>
                     </AtModal>
                 </View>
                 <View className='at-col at-col-.5'></View>
